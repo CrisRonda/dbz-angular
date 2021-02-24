20e77e147871b519118a239cd316c7c988541f40
CrisRonda <cristian.j005@gmail.com>
2021-02-23 20:09:03 -0500

    remove all

--- src/app/models/character.ts
+++
@@ -1,7 +0,0 @@
-export interface Character {
-  id: String;
-  name: String;
-  power: String;
-  image: String;
-  race: String;
-}
--- src/app/models/photo.ts
+++
@@ -1,7 +0,0 @@
-export interface Photo {
-  albumId: Number;
-  id: Number;
-  title: String;
-  url: String;
-  thumbnailUrl: String;
-}
