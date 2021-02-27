04f87fd2bd7498ab49992593145e7da2bb79dcef
CrisRonda <cristian.j005@gmail.com>
2021-02-27 16:11:05 -0500

    styles

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,14 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
   showFiller = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  openSocialLink(sn: string) {
+    let url = '';
+    switch (sn) {
+      case 'fb':
+        url = 'https://www.facebook.com/cristian.ronda2';
+        break;
+      case 'md':
+        url = 'https://cristian-ronda.medium.com/';
+        break;
+      case 'gh':
+        url = 'https://github.com/CristianRonda';
+        break;
+      case 'lk':
+        url = 'https://www.linkedin.com/in/cristian-ronda-169414180/';
+        break;
+      default:
+        url = 'https://cristian-ronda.tk/';
+        break;
+    }
+    window.open(url, '_blank');
   }
-
 }
